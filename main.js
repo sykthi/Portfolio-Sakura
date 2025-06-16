961236cb9f96d3495f8b98f6f1c12d48957feda9
Sakthinivas <nivassakthi2002@gmail.com>
2025-06-17 01:38:59 +0530

    modal

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
@@ -16,18 +17,65 @@ renderer.shadowMap.enabled = true;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1.75;
 
+const modalContent = {
+  "gong":{
+    title: "About",
+    content: "i am",
+  },
+  "shrine":{
+    title: "Project",
+    content: "This is project",
+    link: "https://github.com/sykthi",
+  },
+  "fighting_post":{
+    title: "skill",
+    content: "i have skills",
+  },
+  "statue_frog":{
+    title: "Contact",
+    content: "contact me",
+  },
+};
+
+const modal = document.querySelector(".modal");
+const modalTitle = document.querySelector(".modal-title");
+const modalProjectDescription = document.querySelector(".modal-project-description");
+const modalExitButton = document.querySelector(".modal-exit-button");
+const modalVisitProjectButton = document.querySelector(".modal-project-visit-button");
+
+function showModal(id){
+  const content = modalContent[id];
+  if (content){
+    modalTitle.textContent = content.title;
+    modalProjectDescription.textContent = content.content;
+
+    if(content.link){
+      modalVisitProjectButton.href = content.link
+      modalVisitProjectButton.classList.remove('hidden');
+    }
+    else{
+      modalVisitProjectButton.classList.add('hidden');
+    }
+    modal.classList.toggle("hidden");
+  }
+}
+
+function hideModel() {
+  modal.classList.toggle("hidden");
+}
+
 let intersectObject = "";
 const intersectObjects = [];
 const intersectObjectsNames = [
   "fighting_post",
   "statue_frog",
   "shrine",
-  "gong"
+  "gong",
 ];
 //loading model
 const loader = new GLTFLoader();
 
-loader.load( './Scene1.glb', function ( glb ) {
+loader.load( './Scene.glb', function ( glb ) {
   glb.scene.traverse((child) => {
     if(intersectObjectsNames.includes(child.name)){
       intersectObjects.push(child);
@@ -35,13 +83,12 @@ loader.load( './Scene1.glb', function ( glb ) {
     if(child.isMesh){
       child.castShadow = true;
       child.receiveShadow = true;
-      // console.log(child.material);
       if(child.material.name === "Water")
       {
         child.material.metalness = .7;
         child.material.roughness = 0;
         child.material.transparent = true;
-        child.material.opacity = 0.6;
+        child.material.opacity = 0.7;
       }
     }
     // console.log(child);
@@ -70,10 +117,9 @@ sun.shadow.normalBias = .2;
 scene.add( sun );
 
 const shadowHelper = new THREE.CameraHelper( sun.shadow.camera );
-scene.add( shadowHelper );  //shadow helper
-// console.log(sun.shadow);
+// scene.add( shadowHelper );  //shadow helper
 const helper = new THREE.DirectionalLightHelper( sun, 10);
-scene.add( helper );
+// scene.add( helper );
 
 //Ambilight
 const light = new THREE.AmbientLight( 0x404040, 5); // soft white light
@@ -104,6 +150,9 @@ function onResize(){
 
 function onclick(){
   console.log(intersectObject);
+  if(intersectObject !=="") {
+    showModal(intersectObject);
+  }
 }
 
 function onPointerMove( event ) {
@@ -111,28 +160,33 @@ function onPointerMove( event ) {
 	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
 
+function onkeyDown(event){
+  console.log(event);
+}
+
+modalExitButton.addEventListener("click", hideModel);
 window.addEventListener("resize", onResize);
 window.addEventListener("click", onclick);
-window.addEventListener( 'pointermove', onPointerMove );
+window.addEventListener("pointermove", onPointerMove);
+window.addEventListener("keydown", onkeyDown);
 
 function animate() {
-  raycaster.setFromCamera( pointer, camera );
 
-	const intersects = raycaster.intersectObjects( intersectObjects );
+  raycaster.setFromCamera(pointer, camera);
 
-  if(intersects.length >0){
+  const intersects = raycaster.intersectObjects(intersectObjects);
+
+  if (intersects.length > 0) {
     document.body.style.cursor = "pointer";
-  }
-  else{
+  } else {
     document.body.style.cursor = "default";
     intersectObject = "";
   }
 
-	for ( let i = 0; i < intersects.length; i ++ ) {
-    // console.log(intersects[0].object.parent.name);
-    intersectObject = intersects[0].object.parent.name;
-	}
-  // console.log(camera.position);
+  for (let i = 0; i < intersects.length; i++) {
+    // console.log(intersects[0].object);
+    intersectObject = intersects[0].object.name;
+  }
   renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
