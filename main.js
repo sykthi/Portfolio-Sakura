bd7d3ad69a7d0e3f37794c234d31a655846660f8
Sakthinivas <nivassakthi2002@gmail.com>
2025-07-04 00:06:07 +0530

    zoom 10

--- main.js
+++ main.js
@@ -418,7 +418,7 @@ camera.position.y = 10;
 camera.position.z = -10;
 
 const cameraoffset = new THREE.Vector3( 10, 10,-10);
-camera.zoom = 20;
+camera.zoom = 10;
 camera.updateProjectionMatrix();
 
 // Post-processing setup
